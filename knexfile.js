1d6c46effb9b3fe3118c1d412bd7c50200cc90db
Mark Halls <mark.halls@gmail.com>
2019-12-13 10:08:57 -0700

    Fix db file path

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "../data/project_data.db3"
+      filename: "./data/project_data.db3"
     },
     migrations: {
       directory: "./data/migrations"
